e4a73978e0e580bdebf0f9637816cb9d908f7885
IonHaryono <ion.haryono@gmail.com>
2021-08-26 22:26:25 +1000

    $play works now and some edits to $ready
    $ready doesnt spam pings anymore.
    $play works now

--- main.py
+++ main.py
@@ -229,6 +229,7 @@ async def ready(ctx):
             gameName = "Prop Hunt"
     #send function
         if gameID != "":
+            await ctx.send(gameID)
             messageOutput = (
                 authorName[0].capitalize() + " has started a ready check for: " + gameName + " |  additional notes: ")
         
@@ -253,7 +254,7 @@ async def ready(ctx):
             messageOutput += "total number of players ready: " + str(playerCounterComing - 1)
             messageOutput += "\n"
             messageOutput += "total number of players not coming: " + str(playerCounterNotComing - 1)            
-            messageOutput = (gameID + "\n" + "```prolog" + "\n" + messageOutput + "```")
+            messageOutput = ("\n" + "```prolog" + "\n" + messageOutput + "```")
 
             botMessage = await channel.send(messageOutput)
             for i in range(len(reactionEmojis)):
@@ -300,7 +301,7 @@ async def ready(ctx):
                 messageOutput += "total number of players ready: " + str(playerCounterComing - 1)
                 messageOutput += "\n"
                 messageOutput += "total number of players not coming: " + str(playerCounterNotComing - 1)            
-                messageOutput = (oldGameID + "\n" + "```prolog" + "\n" + messageOutput + "```")
+                messageOutput = ("\n" + "```prolog" + "\n" + messageOutput + "```")
                 await botMessage.edit(messageOutput)           
 
             try:
@@ -497,7 +498,47 @@ async def stop(ctx):
         await voice_client.stop()
     else:
         await ctx.send("The bot is not playing anything at the moment.")
+#=====================
+#Trial play
+#=====================
+@client.command()
+async def playyt(ctx, url):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        em8 = discord.Embed(title = "Music Is Currently Playing", description = 'Please wait for the current playing music to end or use %leave <:_Paimon6:827074349450133524>.\nMusic provided by {ctx.author.mention} <:_Paimon6:827074349450133524>',color = ctx.author.color)
+        await ctx.send(embed = em8)
+        return
 
+    voiceChannel = ctx.message.author.voice.channel
+    await voiceChannel.connect()
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    em6 = discord.Embed(title = "Downloading Youtube Music", description = f'{url}\n\nPlease wait for paimon to setup the music you provide.\nMusic provided by {ctx.author.mention} <:_Paimon6:827074349450133524>',color = ctx.author.color)
+    await ctx.send(embed = em6, delete_after = 2)
+    await ctx.message.delete()
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '196',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, "song.mp3")
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+    em1 = discord.Embed(title = "Now Listening Youtube Music", description = f'{url}\n\nPlease use %leave first to change music.\nMusic provided by {ctx.author.mention} <:_Paimon6:827074349450133524>',color = ctx.author.color)
+
+    videoID = url.split("watch?v=")[1].split("&")[0]
+
+    em1.set_thumbnail(url = f'https://img.youtube.com/vi/{videoID}/default.jpg'.format(videoID = videoID))
+    await ctx.send(embed = em1)
 
 
 """
