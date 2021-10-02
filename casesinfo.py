8af2379296bd47d82ee20494b6c11cae2ce688ce
bradleydhn <89325234+bradleydhn@users.noreply.github.com>
2021-10-03 00:19:36 +1000

    updated casesinfo
    added progress bar, commented out sydney

--- casesinfo.py
+++ casesinfo.py
@@ -8,6 +8,7 @@ from datetime import datetime, date, timedelta
 import datetime as dt
 import numpy as np
 import pytz
+import math
 
 def time_check():
     now = dt.datetime.now()
@@ -29,7 +30,15 @@ def time_check():
 def scrapeCases():
     html_vic = requests.get('https://www.coronavirus.vic.gov.au/').text
     soupM = BeautifulSoup(html_vic, 'lxml')
-    casesMelb = soupM.find('div', class_ = 'ch-daily-update__statistics-item-text').text
+    casesMelb = soupM.find_all('div', class_ = 'ch-daily-update__statistics-item-text')[3].text
+    vaxProgressText = soupM.find_all('div', class_ = 'ch-daily-update__statistics-item-text')[2].text
+    vaxProgress = vaxProgressText.strip('%')
+    fillCount = int(float(vaxProgress)) / 70 * 20
+    roundCount = math.floor(fillCount)
+    block = '\u25a0'
+    emptyBlock = '\u25a1'
+    progressBar = roundCount*block + (20-roundCount)*emptyBlock
+    progressResult = "Lockdown end: " + progressBar + " " + vaxProgressText + " / 70% fully vaccinated"
     melb_tz = pytz.timezone('Australia/Melbourne')
     now = dt.datetime.now()
     now_adj = now.astimezone(melb_tz)
@@ -37,30 +46,32 @@ def scrapeCases():
     timeMelb = now_adj.strftime("%B %d, %Y %I:%M %p")
     # print(timeMelb)
     melbResult = (casesMelb + " cases acquired in Victoria (last 24 hours) (Last updated: " + timeMelb +")")
+    # print(progressResult)
 
     # old = np.load("cases_bruh") 
     # print(old[0])
     # DRIVER_PATH = 'C:\Program Files (x86)\chromedriver.exe'
     # driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-    URL = "https://www.health.nsw.gov.au/Infectious/covid-19/Pages/default.aspx"
-    options = Options()
-    options.headless = True
-    options.add_argument("--window-size=1920,1200")
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--incognito')
-    driver = webdriver.Chrome(options=options)
+    # URL = "https://www.health.nsw.gov.au/Infectious/covid-19/Pages/default.aspx"
+    # options = Options()
+    # options.headless = True
+    # options.add_argument("--window-size=1920,1200")
+    # options.add_argument('--ignore-certificate-errors')
+    # options.add_argument('--incognito')
+    # driver = webdriver.Chrome(options=options)
 
-    # driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
-    driver.get(URL)
-    driver.implicitly_wait(5)
-    page_source = driver.page_source
-    soupS = BeautifulSoup(driver.page_source, 'lxml')
-    casesSyd = soupS.find_all("span", {"class": "number"})[3].text
-    sydResult = (casesSyd + " cases acquired in NSW (last 24 hours)")
-    driver.quit()
+    # # driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
+    # driver.get(URL)
+    # driver.implicitly_wait(5)
+    # page_source = driver.page_source
+    # soupS = BeautifulSoup(driver.page_source, 'lxml')
+    # casesSyd = soupS.find_all("span", {"class": "number"})[3].text
+    # sydResult = (casesSyd + " cases acquired in NSW (last 24 hours)")
+    # driver.quit()
     
-    return [melbResult, sydResult] # returns 2D array
+    return [progressResult, melbResult] # returns 2D array
 
+# scrapeCases()
 
 def mainCases():
     if time_check():
